b4ad1b3fef2c0fea6c250a7c8ef68ea9c82e7ad8
Net-a-ui <neithenrr@gmail.com>
2026-09-18 13:03:20 -0500

    Add Week 2 processing program

---
+++ Week2/processing_employee_data_file.py
@@ -0,0 +1,34 @@
+# Juvenal Nava
+# Programming Assignment 2B
+
+from CommissionWorker import CommissionWorker
+from PieceWorker import PieceWorker
+
+infile = open("employee.txt")
+table = list()
+
+for data in infile:
+    data_field = data.split()
+
+    if data_field[0] == "C":
+        emp = CommissionWorker(int(data_field[1]), data_field[2], data_field[3],
+                               float(data_field[4]), float(data_field[5]),
+                               float(data_field[6]))
+        table.append(emp)
+
+    elif data_field[0] == "P":
+        emp = PieceWorker(int(data_field[1]), data_field[2], data_field[3],
+                          float(data_field[4]), int(data_field[5]))
+        table.append(emp)
+
+print()
+print("Gross-pay salary report")
+print()
+print("%-18s %-6s %-15s %-15s %10s" %
+      ("Employee Type", "ID", "First Name", "Last Name", "Weekly Pay"))
+print("---------------------------------------------------------------------")
+
+for employee in table:
+    print(employee.earnings())
+
+infile.close()
